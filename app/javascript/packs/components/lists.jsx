7d2fef3904eccba5dabdd27b28c2c22f6153add2
yarhtut <yar@boost.co.nz>
2018-02-27 21:58:39 +1300

    remove unsed file

--- app/javascript/packs/components/lists.jsx
+++ app/javascript/packs/components/lists.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import Card from './cards.jsx'
+import Card from './cards'
 
 const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
   const [removed] = result[0][1].splice(startIndex, 1);
-  //result.splice(endIndex, 0, removed);
+  result.splice(endIndex, 0, removed);
 
   return result;
 };
