63d00e248f89b6e1256ea7d2a6bf968e5eacd167
yarhtut <yar@boost.co.nz>
2018-04-19 22:44:08 +1200

    add store

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,18 @@ import Board from './board/board'
 
 import store from './store'
 
-const state = store.getState()
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Board {...state} />,
-    document.body.appendChild(document.getElementById('board'))
-  )
-})
+const render = () => {
+  const state = store.getState()
+
+  document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(
+      <Board {...state} />,
+      document.body.appendChild(document.getElementById('board'))
+    )
+  })
+}
+render()
+
+store.subscribe(render)
+
--- app/javascript/packs/store.js
+++ app/javascript/packs/store.js
@@ -1,5 +1,4 @@
 import { createStore } from 'redux'
 import  reducer from './reducers/list'
 
-
 export default createStore(reducer)
